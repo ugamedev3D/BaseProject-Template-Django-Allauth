e92e6d81f4e4de7b11d2e840018ddc4fef38ea7e
ugamedev3D <ugamedev08@gmail.com>
2026-04-04 00:38:50 +0800

    Django project template for base project setup and with allauth (google/facebook)

---
+++ baseproject/template/src/core/settings/development.py
@@ -0,0 +1,107 @@
+from .allauth import *
+
+import  os
+from environ import Env
+from django.core.management.utils import get_random_secret_key
+
+
+env = Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+env.read_env()
+
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# For development stage use a generate a new random key every server run 
+# for production check production.py 
+SECRET_KEY = get_random_secret_key()
+
+
+# Use install debugger applications  
+INSTALLED_APPS += [
+    'livereload',
+]
+
+MIDDLEWARE += [
+    'livereload.middleware.LiveReloadScript', 
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'VERIFIED_EMAIL': True,
+        'APP': {
+            'client_id': env('OAUTH_GOOGLE_CLIENT_ID'),
+            'secret':  env('OAUTH_GOOGLE_SECRET'),
+        },
+        'SCOPE': {
+            'profile',
+            'email'
+        },
+        'AUTH_PARAMS': {
+            'access_type' : 'online',
+            'prompt' : 'consent'
+        },
+    },
+    'facebook': {
+         'APP': {
+            'client_id': env('OAUTH_FACEBOOK_CLIENT_ID'),
+            'secret':  env('OAUTH_FACEBOOK_SECRET'),
+        },
+        'SCOPE': {
+            'profile',
+        },
+        'AUTH_PARAMS': {
+            'access_type' : 'reauthenticate',
+        },
+    }
+}
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": (
+            "django.contrib.staticfiles.storage.StaticFilesStorage"
+        ),
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Email backend 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
---
+++ baseproject/template/src/core/settings/production.py
@@ -0,0 +1,57 @@
+from .allauth import *
+import os
+DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+
+ALLOWED_HOSTS = []
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
+
+
+# Email backend 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mamadforkids@gmail.com'
+EMAIL_HOST_PASSWORD = 'fnhv ugvb mgxi ngca'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'VERIFIED_EMAIL': True,
+        'APP': {
+            'client_id': os.environ.get('OAUTH_GOOGLE_CLIENT_ID'),
+            'secret':  os.environ.get('OAUTH_GOOGLE_SECRET'),
+        },
+        'SCOPE': {
+            'profile',
+            'email'
+        },
+        'AUTH_PARAMS': {
+            'access_type' : 'online',
+            'prompt' : 'consent'
+        },
+    },
+    'facebook': {
+         'APP': {
+            'client_id': os.environ.get('OAUTH_FACEBOOK_CLIENT_ID'),
+            'secret':  os.environ.get('OAUTH_FACEBOOK_SECRET'),
+        },
+        'SCOPE': {
+            'profile',
+        },
+        'AUTH_PARAMS': {
+            'access_type' : 'reauthenticate',
+        },
+    }
+}
